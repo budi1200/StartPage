c636bb7107ed429de4f6f30f79885fe32124f796
Alen Budimir <7889389+budi1200@users.noreply.github.com>
2022-10-26 22:55:04 +0200

    Update google favicon url

--- src/util/favicon-helper.ts
+++ src/util/favicon-helper.ts
@@ -5,7 +5,8 @@ export const getFaviconUrl = (type: IconType, domain: string) => {
 
     switch (type) {
         case IconType.GOOGLE:
-            url = `https://www.google.com/s2/favicons?sz=192&domain_url=${domain}`;
+            // url = `https://www.google.com/s2/favicons?sz=192&domain_url=${domain}`;
+            url = `https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://${domain}&size=192`;
             break;
         case IconType.DUCKDUCKGO:
             url = `https://external-content.duckduckgo.com/ip3/${domain}.ico`;
